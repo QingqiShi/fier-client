b7ff73281a89bbfa750a899f148b8da3f2ef00b3
Qingqi Shi <qingqishi@live.com>
2019-08-31 16:42:26 +0100

    fix: fix language selector overlap but in landscape mode

--- src/components/app/LanguageSelector.tsx
+++ src/components/app/LanguageSelector.tsx
@@ -1,34 +1,33 @@
 import React, { useState } from 'react';
-import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
 import { MenuItem, Select } from '@material-ui/core';
 import { Locale, locales } from 'stores/i18n';
 import useTexts from 'hooks/useTexts';
 import useLocale from 'hooks/useLocale';
 
-const useStyles = makeStyles((theme: Theme) =>
+const useStyles = makeStyles(() =>
   createStyles({
     emoji: {
       paddingLeft: 5
     },
     languageContainer: {
       position: 'fixed',
-      bottom: 20,
-      left: 0,
-      right: 0,
+      right: 'calc(env(safe-area-inset-left) + 20px)',
+      top: 20,
       textAlign: 'center'
     }
   })
 );
 
 function LanguageSelector({ position }: { position?: 'bottom' }) {
-  const classes = useStyles();
+  const classes = useStyles({ position });
   const [t] = useTexts();
   const { locale, changeLocale } = useLocale();
 
   const [selectedLang, setSelectedLang] = useState<Locale>(locale);
 
   function handleLanguageChange(
-    event: React.ChangeEvent<{ namne?: string; value: unknown }>
+    event: React.ChangeEvent<{ name?: string; value: unknown }>
   ) {
     const newLocale = event.target.value as Locale;
     setSelectedLang(newLocale);
@@ -40,20 +39,20 @@ function LanguageSelector({ position }: { position?: 'bottom' }) {
       className={position === 'bottom' ? classes.languageContainer : undefined}
     >
       <Select
-        value={selectedLang}
-        onChange={handleLanguageChange}
+        name="language"
         renderValue={value => (
           <span className={classes.emoji}>
             {t[`FLAG_${(value as Locale).toUpperCase()}`]}
           </span>
         )}
-        name="language"
+        value={selectedLang}
+        onChange={handleLanguageChange}
       >
         {locales.map(locale => {
           const language = t[`LANGUAGE_${locale.toUpperCase()}`];
           const flag = t[`FLAG_${locale.toUpperCase()}`];
           return (
-            <MenuItem value={locale} key={`settings-language-option-${locale}`}>
+            <MenuItem key={`settings-language-option-${locale}`} value={locale}>
               <span aria-label={language} role="img">
                 {flag} {language}
               </span>
