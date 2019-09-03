d070fbdb2431aa60b7e173e293324e01b84932d9
Qingqi Shi <qingqishi@live.com>
2019-09-04 00:41:24 +0100

    feat: add ability to change name, email and password

--- src/__mocks__/firebase/app.js
+++ src/__mocks__/firebase/app.js
@@ -1,4 +1,5 @@
 let error = false;
+let currentUser = null;
 let authStateChangeCallback = user => {};
 
 export const mockError = e => {
@@ -7,20 +8,27 @@ export const mockError = e => {
 
 export const mockAuthState = user => {
   authStateChangeCallback(user);
+  currentUser = user;
 };
 
-export const authFunctions = {
+export const mockAuthFunctions = {
   createUserWithEmailAndPassword: jest.fn(() =>
-    error ? Promise.reject(error) : Promise.resolve()
+    error ? Promise.reject(error) : Promise.resolve({ user: currentUser })
   ),
   signInWithEmailAndPassword: jest.fn(() =>
     error ? Promise.reject(error) : Promise.resolve()
   ),
-  signOut: jest.fn(() => (error ? Promise.reject(error) : Promise.resolve())),
   onAuthStateChanged: jest.fn(callback => {
     authStateChangeCallback = callback;
   }),
-  languageCode: ''
+  signOut: jest.fn(() => (error ? Promise.reject(error) : Promise.resolve()))
 };
 
-export const auth = jest.fn(() => authFunctions);
+export const auth = jest.fn(() => ({
+  ...mockAuthFunctions,
+  currentUser,
+  languageCode: ''
+}));
+auth.EmailAuthProvider = {
+  credential: jest.fn()
+};
--- src/hooks/useFormInput.ts
+++ src/hooks/useFormInput.ts
@@ -1,13 +1,15 @@
 import { useState } from 'react';
 
-function useFormInput(initialValue = ''): [string, React.FormEventHandler] {
+function useFormInput(
+  initialValue = ''
+): [string, React.FormEventHandler, (val: string) => void] {
   const [value, setValue] = useState(initialValue);
 
   function handleChange(e: React.FormEvent<HTMLInputElement>) {
     setValue((e.target as HTMLInputElement).value);
   }
 
-  return [value, handleChange];
+  return [value, handleChange, setValue];
 }
 
 export default useFormInput;
