2ee4f48c8ef6aea2f8a88fae3048af012a1822d2
Qingqi Shi <qingqishi@live.com>
2019-10-08 23:30:57 +0100

    feat: add icons

--- src/Meta.tsx
+++ src/Meta.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
 import useTexts from 'hooks/useTexts';
+import useRoute from 'hooks/useRoute';
 
 function Meta() {
   const [t] = useTexts();
+  const { getPath } = useRoute();
 
   return (
     <Helmet>
-      <meta content={t['META_DESCRIPTION']} name="description"></meta>
+      <meta content={t['META_DESCRIPTION']} name="description" />
+      <link href={getPath('/manifest.json')} rel="manifest" />
     </Helmet>
   );
 }
