c1197381c06e0d1b726f2e4466f50586c7493d36
Qingqi Shi <qingqishi@live.com>
2020-04-15 22:41:07 +0100

    test: increase test coverage

--- src/components/modals/CreateTransaction.test.tsx
+++ src/components/modals/CreateTransaction.test.tsx
@@ -1,11 +1,64 @@
 import React from 'react';
-import { render } from 'testUtils';
+import { act, fireEvent } from '@testing-library/react';
+import { mockAuthUser, mockFirestore, render } from 'testUtils';
 import CreateTransaction from './CreateTransaction';
 
 test('render form', () => {
-  const { getByLabelText } = render(<CreateTransaction onClose={() => {}} />);
+  const { getByLabelText, getByText } = render(
+    <CreateTransaction onClose={() => {}} />,
+    {
+      userAndSettings: true,
+    }
+  );
+
+  const userId = 'testid';
+  act(() => void mockAuthUser({ uid: userId }));
+  act(() =>
+    mockFirestore(`settings/${userId}`, {
+      locale: 'en',
+      accounts: [
+        { id: 1, name: 'Test Account' },
+        { id: 2, name: 'Another Account' },
+      ],
+    })
+  );
+
   expect(getByLabelText(/Amount/)).toBeInTheDocument();
   expect(getByLabelText('Account')).toBeInTheDocument();
   expect(getByLabelText('Date')).toBeInTheDocument();
   expect(getByLabelText('Notes')).toBeInTheDocument();
+
+  // Amount
+  fireEvent.change(getByLabelText(/Amount/), { target: { value: '12.5' } });
+
+  // Account
+  fireEvent.mouseDown(getByLabelText('Account'));
+  fireEvent.click(getByText('Another Account'));
+
+  // Date
+  fireEvent.click(getByLabelText('Date'));
+  fireEvent.mouseDown(getByText('28'));
+  fireEvent.mouseMove(getByText('28'));
+  fireEvent.mouseUp(getByText('28'));
+  fireEvent.touchStart(getByText('28'));
+  fireEvent.touchMove(getByText('28'));
+  fireEvent.touchEnd(getByText('28'));
+  fireEvent.click(getByText('28'));
+  fireEvent.click(getByText('OK'));
+
+  // Notes
+  fireEvent.change(getByLabelText('Notes'), {
+    target: { value: 'Some notes' },
+  });
+});
+
+test('show error for invalid amount', () => {
+  const { getByLabelText, getByText } = render(
+    <CreateTransaction onClose={() => {}} />
+  );
+
+  expect(getByText('You must enter an amount')).toBeInTheDocument();
+
+  fireEvent.change(getByLabelText(/Amount/), { target: { value: 'invalid' } });
+  expect(getByText("This doesn't look like a number")).toBeInTheDocument();
 });
